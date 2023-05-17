47817d7feaf54d5408a9b6ecb6565e3513355f5c
Empory ~ <oyuneverytime@gmail.com>
2023-05-17 17:00:44 +0300

    .

---
+++ admin/add-admin.php
@@ -0,0 +1,54 @@
+<?php
+include("partials/menu.php");
+ ?>
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Add Admin</h1>
+        <br><br>
+        <form action="" method="POST">
+            <table class="tbl-30">
+                <tr>
+                    <td>Full Name: </td>
+                    <td><input type="text" name="full_name" placeholder="Enter Your Name"></td>
+                </tr>
+                <tr>
+                    <td>Username: </td>
+                    <td>
+                        <input type="text" name="username" placeholder="Your username">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Password: </td>
+                    <td>
+                        <input type="password" name="password" placeholder="Your password">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</div>
+
+
+<?php include("partials/footer.php");?>
+<?php
+    if (isset($_POST["submit"])) {
+        $full_name = $_POST["full_name"];
+        $username = $_POST["username"];
+        $password = md5($_POST["password"]);
+        $sql = "INSERT INTO tbl_admin SET full_name='$full_name',username='$username',password='$password' 
+        ";
+        
+        if ($db_select) {
+            header("location:index.php");
+        }else{
+            echo "wrog";
+        }
+        $res = mysqli_query($conn, $sql) or die(mysqli_error());
+    }
+
+?>
--- admin/manage-order.php
+++ admin/manage-order.php
@@ -2,6 +2,47 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Manage Order</h1>
+        <br /><br />
+                <!-- Button to Add Admin -->
+                <a href="#" class="btn-primary">Add Order</a>
+                <br /><br /><br />
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Vjay</td>
+                        <td>vjay</td>
+                        <td>
+                            <a class="btn-secondary" href="#">Update Admin</a> 
+                            <a class="btn-danger" href="#">Delete Admin</a> 
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Vjay</td>
+                        <td>vjay</td>
+                        <td>
+                            
+                        <a class="btn-secondary" href="#">Update Admin</a> 
+                            <a class="btn-danger" href="#">Delete Admin</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Vjay</td>
+                        <td>vjay</td>
+                        <td>
+                            
+                        <a class="btn-secondary" href="#">Update Admin</a> 
+                            <a class="btn-danger" href="#">Delete Admin</a>
+                        </td>
+                    </tr>
+                </table>
     </div>
 </div>
 <?php include("partials/footer.php"); ?>       
